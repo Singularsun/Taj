894a514d4d0c6178cfad9562de730b240c9afd6a
liyuanyuan <doubleyuan_li@163.com>
2016-07-21 12:16:46 +0800

    addressed comments

--- calc-performance.R
+++ calc-performance.R
@@ -10,8 +10,12 @@ performance_stats <- function(){
   data$partition_day <- paste(substr(data$time.x, start = 1, stop = 10), sep = "")
   #将时间转换为yyyy-mm-ddThh:00格式
   data$partition_hour <- paste(substr(data$time.x,start = 1, stop = 13), ":00", sep = "")
-  daily.result <- sqldf("select partition_day as partition,api,interval,count(interval) as count from data group by api")
-  hourly.result <- sqldf("select partition_hour as partition,api,interval,count(interval) as count from data group by api")
+  daily.result <- sqldf("select partition_day as partition,api,interval,count(interval) as count 
+                        from data group by partition,api,interval")
+  daily.result$granularity <- 'day'
+  hourly.result <- sqldf("select partition_hour as partition,api,interval,count(interval) as count 
+                         from data group by partition,api,interval")
+  hourly.result$granularity <- 'hour'
   result = rbind(daily.result,hourly.result)
   file.path <- paste0("./output/performance-stats/", daily.result[1,'partition'], ".csv")
   write.csv(x = result, file = file.path, row.names = F)
